c246d0e2a838811831b1e5dd923f8df019c09bf3
NGO Stephane <sngo@skillbase.fr>
2023-11-03 13:41:10 +0100

    mise a jour

--- modules/Search.ts
+++ modules/Search.ts
@@ -1,7 +1,18 @@
 import axios from "axios";
 import { KaiStudioCredentials } from "..";
 
+export interface DocumentResult {
+    id: string,
+    name: string,
+    url: string
+}
 
+export interface SearchResult {
+    query: string,
+    answer: string,
+    confidentRate: number,
+    documents: DocumentResult[]
+}
 
 export class Search {
 
@@ -11,7 +22,7 @@ export class Search {
         this.credentials = credentials
     }
 
-    public async query(query: string, user: string): Promise<any> {
+    public async query(query: string, user: string): Promise<SearchResult> {
         try {
             const request = await axios({
                 url: `https://${this.credentials.organizationId}.kai-studio.ai/${this.credentials.instanceId}/api/search/query`,
